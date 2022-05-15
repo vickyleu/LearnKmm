50a05abe2f4f23900de8d4af5b72f601e33700eb
vickyleu <yu6564172@gmail.com>
2022-05-16 00:57:49 +0800

    refactor project structure

--- flutter/ios/Classes/GeyanHybridPlugin.swift
+++ flutter/ios/Classes/GeyanHybridPlugin.swift
@@ -2,10 +2,11 @@ import Flutter
 import shared
 class GeyanHybridPlugin : CommonPluginImpl<PlatformImpl>, FlutterPlugin{
 
-override class func registerWithRegistrar(registrar:NSObject<FlutterPluginRegistrar>){
-    let plugin = GeyanHybridPlugin.init(channelName:"abcdefd")
+ public static func register(with registrar: FlutterPluginRegistrar){
+   let plugin = GeyanHybridPlugin.init(channelName : "abcdefd")
      plugin.registerWithRegistrar(registrar)
-}
+ }
+
 override func createMethodChannel()-> PlatformImpl{
     return PlatformImpl()
 }
