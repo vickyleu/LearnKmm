5022a6bda3f365af1ae4beb330318c8093307ec1
vickyleu <yu6564172@gmail.com>
2022-05-16 01:08:03 +0800

    refactor project structure

--- flutter/ios/Classes/GeyanHybridPlugin.swift
+++ flutter/ios/Classes/GeyanHybridPlugin.swift
@@ -1,5 +1,5 @@
 import Flutter
-import shared
+//import shared
 class GeyanHybridPlugin : CommonPluginImpl<PlatformImpl>, FlutterPlugin{
 
  public static func register(with registrar: FlutterPluginRegistrar){
